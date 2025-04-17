0ba011b841328d03ae847785c96476b99b219edc
DahmanDidiDLR <d.didi@dlr.nl>
2025-04-17 13:05:40 +0200

    small edits the way db is updated

--- backend/cleaners/db_cleaner.sh
+++ backend/cleaners/db_cleaner.sh
@@ -3,37 +3,25 @@
 echo "DB cleanup started" >> /app/backend/cleaners/cron.log
 
 TABLES=(chat file folder)
-QUERY="DELETE FROM %s WHERE created_at < strftime('%s', 'now', '-7 days');"
-sqlite3 /app/backend/data/webui.db <<EOF
-BEGIN TRANSACTION;
-$(for TABLE in "${TABLES[@]}"; do
-  echo "Deleting $TABLE" >> /app/backend/cleaners/cron.log
-  printf "$QUERY\n" "$TABLE"
-done)
-COMMIT;
-EOF
+QUERY="DELETE FROM %s WHERE created_at < unixepoch(datetime('now', '-7 days'));\n"
 
-if [ $? -ne 0 ]; then
-  echo "DB cleanup failed" >> /app/backend/cleaners/cron.log
-  echo "Foutbericht: $?" >> /app/backend/cleaners/cron.log
-  sqlite3 /app/backend/data/webui.db "ROLLBACK;"
-  exit 1
-fi
+for TABLE in "${TABLES[@]}"; do
+ QUERY_WITH_TABLE=$(printf "$QUERY" "$TABLE")
+ sqlite3 /app/backend/data/webui.db "$QUERY_WITH_TABLE"
+ if [ $? -ne 0 ]; then
+  echo "Error executing query on table $TABLE" >> /app/backend/cleaners/cron.log
+ fi
+done
 
 echo "DB cleanup finished" >> /app/backend/cleaners/cron.log
 
 echo "DB Vacuum started" >> /app/backend/cleaners/cron.log
 
 # Om vrijgekomen ruimte vrij te maken en de database te optimaliseren
-sqlite3 /app/backend/data/webui.db <<EOF
-BEGIN TRANSACTION;
-VACUUM;
-COMMIT;
-EOF
+sqlite3 /app/backend/data/webui.db "VACUUM;"
 
 if [ $? -ne 0 ]; then
   echo "DB Vacuum failed" >> /app/backend/cleaners/cron.log
-  exit 1
 fi
 
 echo "DB Vacuum finished" >> /app/backend/cleaners/cron.log
